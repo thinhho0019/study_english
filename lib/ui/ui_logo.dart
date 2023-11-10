0d3eecb28baed91eda303522317160976ec3abab
trongNghiakmn <nghiatrongnguyen26072001@gmail.com>
2023-11-10 14:42:47 +0700

    Mô tả commit

--- lib/ui/ui_logo.dart
+++ lib/ui/ui_logo.dart
@@ -5,10 +5,26 @@ class mainLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Text("Xin chào các bạn"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Color(0xFFE5F7FF), // Màu nền #e5f7ff
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Trường TH&THCS Vĩnh Khương",
+                  style: TextStyle(fontSize: 15, color: Color(0xFF09569a)),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  "Study English",
+                  style: TextStyle(fontSize: 40, color: Color(0xFF09569a)),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/ui/ui_manghinhchinh.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class mainManghinhchinh extends StatelessWidget {
+  const mainManghinhchinh({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFE5F7FF),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 60),
+              Text(
+                "Choose a topic",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    filled: true,
+                    fillColor: Colors.white, // Màu nền trắng
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                    suffixIcon: Icon(Icons.search), // Hoặc Text('Tìm kiếm')
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          'Item $index',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
